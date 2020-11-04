baf5b2c4a781efd93e7917e52d5a353ad29ec1bc
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:13:03 +0700

    Add files via upload

---
+++ PBOIF2_10116378_LATIHAN13_Relational/src/pboif2_10116378_latihan13_relational/PBOIF2_10116378_LATIHAN13_Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116378_latihan13_relational;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas : PBO-IF2
+ * Nim   : 10116378
+ */
+public class PBOIF2_10116378_LATIHAN13_Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b =  " + (bil1 == bil2));
+        System.out.println("a != b =  " + (bil1 != bil2));
+        System.out.println("a > b =  " + (bil1 > bil2));
+        System.out.println("a < b =  " + (bil1 < bil2));
+        System.out.println("b >= a =  " + (bil1 >= bil2));
+        System.out.println("b <= a =  " + (bil1 <= bil2));
+    }
+    
+}
